c360ddd53085f2c58316d696052cf9b80937089b
ubaidz <hayria76@gmail.com>
2020-07-24 14:39:28 +0700

    format example

--- resources/views/admin/content/form.blade.php
+++ resources/views/admin/content/form.blade.php
@@ -54,6 +54,17 @@
                 </select>
             </div>
             <div class="form-group">
+                <label>Format</label>
+                <select class="form-control select2" name="format" id="format" style="width: 100%;">
+                    @for($i=1;$i<=10;$i++)
+                    <option value="{{ $i }}" {{ $item && $item['content']->format == $i ? 'selected="selected"' : '' }}>Format - {{ $i }}</option>
+                    @endfor
+                </select>
+                <div id="imgshow" style="margin-top:15px;max-height:100px;">
+                    <img src="{{ url('/format/format_1.png') }}" name="image-swap" style="width:10%;">
+                </div>
+            </div>
+            <div class="form-group">
                 <label>Category</label>
                 <select class="form-control select2" name="category_id" style="width: 100%;">
                     @foreach($category as $row)
@@ -115,6 +126,19 @@
                     </span>
                     <input id="thumbnail" class="form-control" type="text" name="filepath">
                 </div>
+                <div id="holder" style="margin-top:15px;max-height:100px;"></div>
+            </div>
+            <div class="form-group" id="img">
+                <label>Image Icon</label>
+                <div class="input-group">
+                    <span class="input-group-btn">
+                        <a id="lfm2" data-input="thumbnail2" data-preview="holder2" class="btn btn-primary text-white">
+                        <i class="fa fa-picture-o"></i> Choose
+                        </a>
+                    </span>
+                    <input id="thumbnail2" class="form-control" type="text" name="icon">
+                </div>
+                <div id="holder2" style="margin-top:15px;max-height:100px;"></div>
             </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">Link Video</label>
@@ -143,6 +167,8 @@
 <!-- TinyMCE init -->
 <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
 <script>
+    var route_prefix = "{{ url('/filemanager') }}";
+    var route_img = "{{ url('/') }}";
     var editor_config = {
         directionality: document.dir,
         path_absolute: "{{ url('/') }}"+"/",
@@ -210,6 +236,42 @@
     tinymce.init(editor_configEn);
     {!! \File::get(base_path('vendor/unisharp/laravel-filemanager/public/js/stand-alone-button.js')) !!}
     $('#lfm').filemanager('image', {prefix: "{{ url('/') }}"+"/laravel-filemanager"});
+
+    var lfm = function(id, type, options) {
+      let button = document.getElementById(id);
+
+      button.addEventListener('click', function () {
+        var route_prefix = (options && options.prefix) ? options.prefix : '/filemanager';
+        var target_input = document.getElementById(button.getAttribute('data-input'));
+        var target_preview = document.getElementById(button.getAttribute('data-preview'));
+
+        window.open(route_prefix + '?type=image', 'FileManager', 'width=900,height=600');
+        window.SetUrl = function (items) {
+          var file_path = items.map(function (item) {
+            return item.url;
+          }).join(',');
+
+          // set the value of the desired input to image url
+          target_input.value = file_path;
+          target_input.dispatchEvent(new Event('change'));
+
+          // clear previous preview
+          target_preview.innerHtml = '';
+
+          // set or change the preview image src
+          items.forEach(function (item) {
+            let img = document.createElement('img')
+            img.setAttribute('style', 'height: 5rem')
+            img.setAttribute('src', item.thumb_url)
+            target_preview.appendChild(img);
+          });
+
+          // trigger change event
+          target_preview.dispatchEvent(new Event('change'));
+        };
+      });
+    };
+    lfm('lfm2', 'file', {prefix: route_prefix});
     $(function () {
         //Initialize Select2 Elements
         $('.select2').select2();
@@ -224,6 +286,10 @@
             $("#titledesc").hide();
             $("#img").show();
         }
+        $("#format").change(function(){
+            console.log('val->',$(this).val());
+            $("img[name=image-swap]").attr("src",route_img + '/format/format_' + $(this).val() + '.png' );
+        });
     })
     function checkType() {
         var x = document.getElementById("type").value;
