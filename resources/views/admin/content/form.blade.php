52b8e881d67874874b352c24b756e2dff552e49c
ubaidz <hayria76@gmail.com>
2020-07-25 21:26:57 +0700

    img  url body description

--- resources/views/admin/content/form.blade.php
+++ resources/views/admin/content/form.blade.php
@@ -213,6 +213,8 @@
         "link image code"
         ],
         relative_urls: false,
+        remove_script_host : false,
+        convert_urls : true,
         height: 300,
         file_browser_callback : function(field_name, url, type, win) {
         var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
@@ -224,14 +226,14 @@
         } else {
             cmsURL = cmsURL + "&type=Files";
         }
-
+        
         tinyMCE.activeEditor.windowManager.open({
             file : cmsURL,
             title : 'Filemanager',
             width : x * 0.8,
             height : y * 0.8,
             resizable : "yes",
-            close_previous : "no"
+            close_previous : "no",
         });
         }
     };
@@ -246,6 +248,8 @@
         "link image code"
         ],
         relative_urls: false,
+        remove_script_host : false,
+        convert_urls : true,
         height: 300,
         file_browser_callback : function(field_name, url, type, win) {
         var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
