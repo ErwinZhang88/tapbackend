c7f1fcbe9fd60e0af1479d1851a49b984b675d28
fahmi <hayria76@gmail.com>
2020-09-18 15:20:40 +0700

    latest-report

--- app/Http/Controllers/API/MenuController.php
+++ app/Http/Controllers/API/MenuController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\MenuId;
 use App\Models\MenuEn;
+use DB;
 use App\Category;
 use App\ContentPost;
 use App\ContentPostTranslation;
@@ -47,9 +48,9 @@ class MenuController extends BaseController
         }
         // echo $nicename;die;
         if($lang == 'en'){
-            $menus = MenuEn::select('id','nameEn as name','banner','banner_mobile')->where('nicenameEn',$nicename)->first();
+            $menus = MenuEn::select('id','nameEn as name','banner','banner_mobile','type')->where('nicenameEn',$nicename)->first();
         }else{
-            $menus = MenuId::select('id','name','banner','banner_mobile')->where('nicename',$nicename)->first();
+            $menus = MenuId::select('id','name','banner','banner_mobile','type')->where('nicename',$nicename)->first();
         }
         if($menus){
             if($lang == 'en'){
@@ -63,46 +64,89 @@ class MenuController extends BaseController
                 foreach($category as $row){
                     $row['content'] = array();
                     $order = $row->is_sort == 1 ? 'asc' : 'desc';
-                    if($row->pagination == 1){
-                        $content = ContentPost::where('category_id',$row->id)->OrderBy('id',$order)->paginate($row->limitpage);
-                        $row->contentTotal	= $content->count();
-                        $row->current_page 	= $content->currentPage();
-                        $row->lastPage 	    = $content->lastPage();
-                    }else{
-                        $content = ContentPost::where('category_id',$row->id)->OrderBy('id',$order)->get();
-                        $row->contentTotal = 0;
-                        $row->current_page = 0;
-                        $row->lastPage = 0;
-                    }
-                    if($content){
-                        $contentrow = array();
-                        foreach($content as $rowcontent){
-                            $contentData = ContentPostTranslation::where('locale',$lang)->where('content_post_id',$rowcontent->id)->first();
-                            // dd($contentData);
-                            $createdAt = Carbon::parse($rowcontent->created_at);
-                            $contentrow[] = array(
-                                'id' => $rowcontent->id,
-                                'type' => $rowcontent->type,
-                                'format' => $rowcontent->format,
-                                'bg_color' => $rowcontent->bg_color,
-                                'button' => $rowcontent->button,
-                                'title' => $rowcontent->name,
-                                'show_title' => $rowcontent->show_title,
-                                'image' => $rowcontent->images,
-                                'image_mobile' => $rowcontent->image_mobile,
-                                'icon' => $rowcontent->icon,
-                                'files' => $rowcontent->files,
-                                'is_download' => $rowcontent->is_download,
-                                'video' => $rowcontent->video,
-                                'created_at' => $createdAt->format('d F Y'),
-                                'createdAt' => $createdAt->format('Y F'),
-                                'title' => $contentData ? $contentData->name : '',
-                                'nicename' => $contentData ? $contentData->nicename : '',
-                                'desc' => $contentData ? $contentData->description : '',
-                                'short_desc' => $contentData ? $contentData->short_desc : ''
-                            );
+                    if($row->is_sort == 3){
+                        $menusParent = MenuEn::select('id')->where('parent_menu_id',$menus->id)->where('type',0)->get()->pluck('id');
+                        $categoryLatest = Category::whereIn('menu_id',$menusParent)->get()->pluck('id');
+                        $content = ContentPost::join('categories','categories.id','=','content_posts.category_id');
+                        if($lang == 'en'){
+                            $content = $content->select(DB::raw('content_posts.*, max(content_posts.id) as id,categories.name as name'));
+                        }else{
+                            $content = $content->select(DB::raw('content_posts.*, max(content_posts.id) as id,categories.nameEn as name'));
                         }
-                        $row['content'] = $contentrow;
+                        $content = $content->whereIn('category_id',$categoryLatest)->groupBy('category_id')->OrderBy('id',$order)->get();
+                        // dd($content);
+                        if($content){
+                            $contentrow = array();
+                            foreach($content as $rowcontent){
+                                $contentData = ContentPostTranslation::where('locale',$lang)->where('content_post_id',$rowcontent->id)->first();
+                                // dd($contentData);
+                                $createdAt = Carbon::parse($rowcontent->created_at);
+                                $contentrow[] = array(
+                                    'id' => $rowcontent->id,
+                                    'type' => $rowcontent->type,
+                                    'format' => $rowcontent->format,
+                                    'bg_color' => $rowcontent->bg_color,
+                                    'button' => $rowcontent->button,
+                                    'title' => $rowcontent->name,
+                                    'show_title' => $rowcontent->show_title,
+                                    'image' => $rowcontent->images,
+                                    'image_mobile' => $rowcontent->image_mobile,
+                                    'icon' => $rowcontent->icon,
+                                    'files' => $rowcontent->files,
+                                    'is_download' => $rowcontent->is_download,
+                                    'video' => $rowcontent->video,
+                                    'created_at' => $createdAt->format('d F Y'),
+                                    'createdAt' => $createdAt->format('Y F'),
+                                    'title' => $rowcontent ? $rowcontent->name : '',
+                                    'nicename' => $contentData ? $contentData->nicename : '',
+                                    'desc' => $contentData ? $contentData->description : '',
+                                    'short_desc' => $contentData ? $contentData->short_desc : ''
+                                );
+                            }
+                            $row['content'] = $contentrow;
+                        }
+                    }else{
+                        if($row->pagination == 1){
+                            $content = ContentPost::where('category_id',$row->id)->OrderBy('id',$order)->paginate($row->limitpage);
+                            $row->contentTotal	= $content->count();
+                            $row->current_page 	= $content->currentPage();
+                            $row->lastPage 	    = $content->lastPage();
+                        }else{
+                            $content = ContentPost::where('category_id',$row->id)->OrderBy('id',$order)->get();
+                            $row->contentTotal = 0;
+                            $row->current_page = 0;
+                            $row->lastPage = 0;
+                        }
+                        if($content){
+                            $contentrow = array();
+                            foreach($content as $rowcontent){
+                                $contentData = ContentPostTranslation::where('locale',$lang)->where('content_post_id',$rowcontent->id)->first();
+                                // dd($contentData);
+                                $createdAt = Carbon::parse($rowcontent->created_at);
+                                $contentrow[] = array(
+                                    'id' => $rowcontent->id,
+                                    'type' => $rowcontent->type,
+                                    'format' => $rowcontent->format,
+                                    'bg_color' => $rowcontent->bg_color,
+                                    'button' => $rowcontent->button,
+                                    'title' => $rowcontent->name,
+                                    'show_title' => $rowcontent->show_title,
+                                    'image' => $rowcontent->images,
+                                    'image_mobile' => $rowcontent->image_mobile,
+                                    'icon' => $rowcontent->icon,
+                                    'files' => $rowcontent->files,
+                                    'is_download' => $rowcontent->is_download,
+                                    'video' => $rowcontent->video,
+                                    'created_at' => $createdAt->format('d F Y'),
+                                    'createdAt' => $createdAt->format('Y F'),
+                                    'title' => $contentData ? $contentData->name : '',
+                                    'nicename' => $contentData ? $contentData->nicename : '',
+                                    'desc' => $contentData ? $contentData->description : '',
+                                    'short_desc' => $contentData ? $contentData->short_desc : ''
+                                );
+                            }
+                            $row['content'] = $contentrow;
+                        }
                     }
                     $content_category[] = $row;
                 }
